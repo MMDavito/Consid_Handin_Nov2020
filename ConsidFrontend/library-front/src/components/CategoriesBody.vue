b7ecd5c766f57f2d1b6b377c965753c14bf8c07d
MMDavito <davidking9753@gmail.com>
2020-11-07 17:23:27 +0100

    Can now CRUD from frontend. Have only implemented Categories. Will tomorrow start with REST-api the shit out of library-items.
    Thereafter I will do frontend of it, this frontend will contain comments (as opposed to the Categorie-frontend)
    After this I will do the employee-part of the application.
    
    The employee-part is not verry different (just a tiny bit more buisness logic)

--- ConsidFrontend/library-front/src/components/CategoriesBody.vue
+++ ConsidFrontend/library-front/src/components/CategoriesBody.vue
@@ -1,103 +1,54 @@
 <template>
     <v-container>
         <v-layout column>
-            <div class="headline mt-3">Empty placeholder before delete</div>
-
             <v-flex class="display-2 text-xs-center my-5"
-                >Big Title Goes Here</v-flex
+                >Empty placeholder before delete</v-flex
             >
             <v-flex>
                 <ul id="Categories">
-                    <li v-for="c in categories" :key="c.category">
-                        {{ c.category }} With id {{ c.id }}
+                    <li v-for="(c, index) in categories" :key="c.category">
+                        {{ c.category }} With id {{ c.id }} and index
+                        {{ index }}
                     </li>
                 </ul>
-                <div v-for="c in categories" :key="c.category">
+                <div v-for="(c, index) in categories" :key="c.category">
                     <label>{{ c.category }}</label
                     ><br />
-                    <input type="text" name="c.category" v-model="c.category" />
-                    <v-btn color="primary" class="update" :name="c.id">Update</v-btn>
-                    <v-btn color="error" class="delete" :name="c.id">Delete</v-btn>
+                    <input
+                        class="input"
+                        :id="index"
+                        type="text"
+                        name="c.category"
+                        :value="c.category"
+                    />
+                    <v-btn
+                        color="primary"
+                        class="update"
+                        :name="index"
+                        @click="update(index)"
+                        >Update</v-btn
+                    >
+                    <v-btn
+                        color="error"
+                        class="delete"
+                        :name="index"
+                        @click="remove(index)"
+                        >Delete</v-btn
+                    >
                 </div>
-            </v-flex>
-            <v-flex>
-                <div class="headline mt-3">Lorem ipsum</div>
-                <p class="subheading mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras pulvinar risus quis mauris interdum, in euismod nibh
-                    pretium. Etiam pulvinar tincidunt dapibus. Quisque
-                    sollicitudin, mauris a consequat consectetur, turpis nisl
-                    sollicitudin enim, id consectetur neque neque nec metus.
-                    Pellentesque dolor nisi, vulputate quis lobortis ac,
-                    tincidunt et quam. Mauris pulvinar blandit nisi nec mattis.
-                    Aliquam accumsan ut sem eget efficitur. Vivamus in tortor
-                    gravida eros laoreet condimentum nec vel dui. Nullam quam
-                    massa, ultrices eget tincidunt a, pulvinar ac libero.
-                </p>
-            </v-flex>
-            <v-flex>
-                <div class="headline mt-3">Lorem ipsum</div>
-                <p class="subheading mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras pulvinar risus quis mauris interdum, in euismod nibh
-                    pretium. Etiam pulvinar tincidunt dapibus. Quisque
-                    sollicitudin, mauris a consequat consectetur, turpis nisl
-                    sollicitudin enim, id consectetur neque neque nec metus.
-                    Pellentesque dolor nisi, vulputate quis lobortis ac,
-                    tincidunt et quam. Mauris pulvinar blandit nisi nec mattis.
-                    Aliquam accumsan ut sem eget efficitur. Vivamus in tortor
-                    gravida eros laoreet condimentum nec vel dui. Nullam quam
-                    massa, ultrices eget tincidunt a, pulvinar ac libero.
-                </p>
-
-                <p class="subheading mt-3">
-                    Nullam nec massa eu est fringilla lobortis. Praesent in enim
-                    in justo blandit varius. Cras placerat arcu in sapien
-                    rhoncus aliquet. Sed interdum tortor et tincidunt
-                    condimentum. Etiam consequat mi leo, in suscipit odio
-                    scelerisque molestie. Nam et purus consequat, iaculis augue
-                    vel, sagittis ligula. Vestibulum aliquet vulputate erat.
-                    Phasellus id mauris mauris. Nunc a maximus dolor. Curabitur
-                    ut vestibulum arcu. Curabitur non lacus justo. Cras varius a
-                    magna in semper. Nulla eros ante, consectetur faucibus
-                    sapien eu, rhoncus imperdiet dui. Sed viverra iaculis nunc,
-                    id pulvinar massa egestas vitae.
-                </p>
-
-                <p class="subheading mt-3">
-                    Aenean erat metus, imperdiet eget nisl laoreet, venenatis
-                    ultricies ante. In interdum ante vel dictum ullamcorper.
-                    Pellentesque habitant morbi tristique senectus et netus et
-                    malesuada fames ac turpis egestas. Integer sit amet gravida
-                    diam. Aliquam in tempor metus. Fusce pellentesque pharetra
-                    sem, et luctus justo tempor dictum. Ut feugiat est sed
-                    tristique egestas. Nullam posuere a nunc in blandit.
-                    Vestibulum ante ipsum primis in faucibus orci luctus et
-                    ultrices posuere cubilia Curae; Suspendisse laoreet ultrices
-                    eros, nec malesuada enim semper sit amet. Maecenas efficitur
-                    consectetur accumsan. Etiam in aliquam turpis, ut pharetra
-                    nulla. Vestibulum malesuada, nulla id elementum cursus, nibh
-                    dui rhoncus felis, suscipit mattis felis enim sed ex.
-                    Pellentesque scelerisque aliquam lorem, vel mattis nibh
-                    tincidunt ac. Suspendisse ac nibh sit amet lacus ullamcorper
-                    maximus.
-                </p>
-            </v-flex>
-            <v-flex>
-                <div class="headline mt-3">Lorem ipsum</div>
-                <p class="subheading mt-3">
-                    Nullam nec massa eu est fringilla lobortis. Praesent in enim
-                    in justo blandit varius. Cras placerat arcu in sapien
-                    rhoncus aliquet. Sed interdum tortor et tincidunt
-                    condimentum. Etiam consequat mi leo, in suscipit odio
-                    scelerisque molestie. Nam et purus consequat, iaculis augue
-                    vel, sagittis ligula. Vestibulum aliquet vulputate erat.
-                    Phasellus id mauris mauris. Nunc a maximus dolor. Curabitur
-                    ut vestibulum arcu. Curabitur non lacus justo. Cras varius a
-                    magna in semper. Nulla eros ante, consectetur faucibus
-                    sapien eu, rhoncus imperdiet dui. Sed viverra iaculis nunc,
-                    id pulvinar massa egestas vitae.
-                </p>
+                <div>
+                    <label>Add new category:</label><br />
+                    <input
+                        placeholder="Fill here"
+                        class="input"
+                        id="new_cat"
+                        type="text"
+                        name="new_cat"
+                    />
+                    <v-btn color="primary" class="add" @click="add()"
+                        >Create</v-btn
+                    >
+                </div>
             </v-flex>
         </v-layout>
     </v-container>
@@ -119,24 +70,208 @@ export default {
         };
 
         fetch('https://127.0.0.1:5001/category/all', requestOptions)
-            .then(response => response.text())
+            .then(response => response.json())
             .then(result => {
                 console.log(result);
-                var obj = JSON.parse(result);
-                console.log(obj);
-                this.categories = obj;
+                this.categories = result;
             })
             .catch(error => console.log('error', error));
-        /*
-        console.log('Will now do pokemon:');
-        fetch('https://pokeapi.co/api/v2/pokemon/?limit=151', requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                var obj = JSON.parse(result);
-                this.pokemon = obj.results;
-            })
-            .catch(error => console.log('error', error));
-            */
+    },
+    methods: {
+        update: function(clicked_id) {
+            console.log('Clicked: ' + clicked_id);
+            var input_field = document.getElementById(clicked_id);
+            console.log(input_field);
+            var newData = input_field.value;
+            console.log(newData);
+            console.log(this.categories);
+            var tempCat = this.categories[clicked_id];
+            console.log('New data: ' + newData);
+            console.log('Old data: ' + tempCat.category);
+            //"check input" but realy just change color of input field and output info to log.
+            //Possible that alert would be better, or something else, but alert is terrible when developing
+            //Next time i will possibly use alert "if not HelperGlobalVariable.ISDEVELOPING is false"
+            if (
+                newData == null ||
+                newData == undefined ||
+                !(newData.length > 0) ||
+                newData.length > 200 ||
+                newData == tempCat.category
+            ) {
+                console.log(
+                    'New category cannot have an empty name or a name longer than 200 bytes.\nNore can it be the same as already existing'
+                );
+                document.getElementById(clicked_id).style.backgroundColor =
+                    'red';
+                return;
+            } else if (tempCat.id == null) {
+                //Following is caused by lazzyness when creating .net response api:
+                document.getElementById(clicked_id).style.backgroundColor =
+                    'red';
+                alert(
+                    'You must reload page before updateing newely created category.'
+                );
+                return;
+            } else {
+                document.getElementById(
+                    clicked_id
+                ).style.backgroundColor = null;
+            }
+            var myHeaders = new Headers();
+            myHeaders.append('Content-Type', 'application/json');
+
+            //var category = null;
+            console.log(tempCat);
+            tempCat.category = newData;
+            console.log(tempCat);
+            var raw = JSON.stringify(tempCat);
+            console.log('New category is: ' + raw);
+            raw = JSON.stringify(raw);
+            console.log('New category is: ' + raw);
+
+            var requestOptions = {
+                method: 'PUT',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            var url = 'https://127.0.0.1:5001/category/' + tempCat.id;
+            fetch(url, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    console.log(result);
+                    console.log('Status var: ' + result.statusCode);
+                    if (result.statusCode != 200) {
+                        console.log('Will change colur');
+                        console.log(document.getElementById(clicked_id));
+                        document.getElementById(
+                            clicked_id
+                        ).style.backgroundColor = 'red';
+                        console.log(document.getElementById(clicked_id));
+                    } else {
+                        this.$set(this.categories, clicked_id, {
+                            id: tempCat.id,
+                            category: tempCat.category
+                        });
+                        document.getElementById(
+                            clicked_id
+                        ).style.backgroundColor = null;
+                    }
+                })
+                .catch(error => console.log('HELVETES ERROR SKIT', error));
+        },
+        add: function() {
+            var input_field = document.getElementById('new_cat');
+            var newData = input_field.value;
+            if (
+                newData == null ||
+                newData == undefined ||
+                !(newData.length > 0) ||
+                newData.length > 200
+            ) {
+                console.log(
+                    'New category cannot have an empty name or a name longer than 200 bytes.\nNore can it be the same as already existing'
+                );
+                document.getElementById('new_cat').style.backgroundColor =
+                    'red';
+                return;
+            } else {
+                document.getElementById('new_cat').style.backgroundColor = null;
+            }
+
+            var myHeaders = new Headers();
+            myHeaders.append('Content-Type', 'application/json');
+            var cat = { id: null, category: newData };
+            console.log('Cat: ' + cat);
+            var raw = JSON.stringify(cat);
+            console.log('raw: ' + raw);
+            raw = JSON.stringify(raw);
+
+            console.log('raw: ' + raw);
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch('https://127.0.0.1:5001/category', requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    result = JSON.parse(result);
+                    console.log(result);
+                    console.log('STATUS FAN: ' + result.statusCode);
+                    if (result.statusCode != 201) {
+                        document.getElementById(
+                            'new_cat'
+                        ).style.backgroundColor = 'red';
+                        return;
+                    } else {
+                        document.getElementById(
+                            'new_cat'
+                        ).style.backgroundColor = null;
+                        this.categories.push(cat);
+                    }
+                })
+                .catch(error => console.log('error', error));
+        },
+        remove: function(clicked_id) {
+            var input_field = document.getElementById(clicked_id);
+            console.log(input_field);
+            var newData = input_field.value;
+            console.log(newData);
+            console.log(this.categories);
+            var tempCat = this.categories[clicked_id];
+            console.log('New data: ' + newData);
+            console.log('Old data: ' + tempCat.category);
+            //"check input" but realy just change color of input field and output info to log.
+            //Possible that alert would be better, or something else, but alert is terrible when developing
+            //Next time i will possibly use alert "if not HelperGlobalVariable.ISDEVELOPING is false"
+            if (
+                newData == null ||
+                newData == undefined ||
+                newData.length != 0
+            ) {
+                alert(
+                    'To delete one must empty the textfield for the item one want to delete.\n\nThis is instead of confirmation.'
+                );
+                document.getElementById(clicked_id).style.backgroundColor =
+                    'red';
+                return;
+            } else if (tempCat.id == null) {
+                //Following is caused by lazzyness when creating .net response api:
+                document.getElementById(clicked_id).style.backgroundColor =
+                    'red';
+                alert(
+                    'You must reload page before deleteing newely created category.'
+                );
+                return;
+            } else {
+                document.getElementById(
+                    clicked_id
+                ).style.backgroundColor = null;
+            }
+            var requestOptions = {
+                method: 'DELETE',
+                redirect: 'follow'
+            };
+            var url = 'https://127.0.0.1:5001/category/' + tempCat.id;
+            fetch(url, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    console.log(result);
+                    result = JSON.parse(result);
+                    if (result.statusCode != 200) {
+                        document.getElementById(
+                            clicked_id
+                        ).style.backgroundColor = 'red';
+                    }else{
+                        this.categories.splice(clicked_id,1)
+                    }
+                })
+                .catch(error => console.log('error', error));
+        }
     }
 };
 </script>
